2021204c997cfa37553f48628baa196ef7321e66
Duaa3 <Duaahil44@gmail.com>
2023-04-02 10:11:25 +0400

    All Students

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.sql.*;
 
 public class Main {
         public static void main(String[] args) {
-            // This line prints a message to the console
             System.out.println("Display All Students");
 
             try {
