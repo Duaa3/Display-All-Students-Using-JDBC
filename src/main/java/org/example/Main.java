7c798fe6bbe31fb0c6dd9810b621ab6f92ad3ab2
Duaa3 <Duaahil44@gmail.com>
2023-04-02 11:24:24 +0400

    All Students

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,39 @@
 package org.example;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-        public static void main(String[] args) {
-            System.out.println("Display All Students");
+    public static void main(String[] args) {
+        System.out.println("Display All Students");
 
-            try {
-                // This line establishes a connection to the database
-                Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/tradb","user","pass");
-                // This line creates a statement object
-                Statement statement = connection.createStatement();
-                // This line executes a SELECT query on the "student" table
-                statement.execute("SELECT * from student;");
-                // This line retrieves the result set of the query
-                ResultSet rs = statement.getResultSet();
-                // This loop iterates over the result set and prints the values to the console
-                while (rs.next()){
-                    int id = rs.getInt("id");
-                    String name = rs.getNString("name");
-                    String email = rs.getNString("email");
-                    System.out.println("ID: "+ id + " , Name: " + name + ",Email: " + email);
-                }
-            } catch (SQLException e) {
-                // This line throws a runtime exception if an error occurs
-                throw new RuntimeException(e);
+        try {
+            // This line establishes a connection to the database
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/tradb","user","pass");
+            // This line creates a statement object
+            Statement statement = connection.createStatement();
+            // This line executes a SELECT query on the "student" table
+            statement.execute("SELECT * from student;");
+            // This line retrieves the result set of the query
+            ResultSet rs = statement.getResultSet();
+            // This list will store the student objects
+            List<Student> studentList = new ArrayList<>();
+            // This loop iterates over the result set and creates Student objects
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getNString("name");
+                String email = rs.getNString("email");
+                Student student = new Student(id, name, email);
+                studentList.add(student);
             }
-
-
+            // Now you can use the studentList as needed, for example:
+            for (Student student : studentList) {
+                System.out.println("ID: "+ student.getId() + " , Name: " + student.getName() + ", Email: " + student.getEmail());
+            }
+        } catch (SQLException e) {
+            // This line throws a runtime exception if an error occurs
+            throw new RuntimeException(e);
         }
-
     }
+}
